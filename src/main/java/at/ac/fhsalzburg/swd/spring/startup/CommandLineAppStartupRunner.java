ee325151da9276322908f1a1ca4dac4d8678cf12
Litharshi <lsivarasa@stud.hs-bremen.de>
2022-01-18 14:46:48 +0100

    TableofCars

--- src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
+++ src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
@@ -37,12 +37,19 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     public void run(String...args) throws Exception {
 
         customerService.addCustomer("Max", "Mustermann", "Puch Urstein", "23","male","+40723571234", "max@muster.man");
+        customerService.addCustomer("Ioana", "Lazea", "Salzburg", "21","female","+40726871238", "lazea@ioana.man");
+        customerService.addCustomer("Sandro", "Sulkni", "Wien", "21","male","+40723395634", "sandro@sulki.man");
+        customerService.addCustomer("Litharshi", "Siva", "Bremen", "24","female","+40793771232", "litharshi@siva.man");
+        customerService.addCustomer("Alex", "Meier", "Hamburg", "26","male","+40723293864", "alex@meier.man");
 
     	Customer customer = customerService.getAll().iterator().next();
     	customer = customerService.getById(1l);
 
 
-    	carService.addCar("Benz", "SUVs", "Automatic","Unlimited ", 5, "Volkswagen Golf or similar", 66 );
+    	carService.addCar("Benz", "SUVs", "Automatic","Unlimited ", 5, "available", 66 );
+        carService.addCar("VW", "Passenger Vans", "Manual","Limited ", 4, "not available", 80 );
+        carService.addCar("BMW", "Cars", "Manual","Unlimited ", 4, "available", 40 );
+        carService.addCar("Polo", "Passenger Vans", "Automatic","Limited ", 5, "not available", 100 );
 
         Car car = carService.getAll().iterator().next();
         //car = carService.getById(2l);
